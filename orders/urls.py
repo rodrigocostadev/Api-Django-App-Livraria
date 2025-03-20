2cc8507c7dae6f92e93288f06b81a7dc9b4f4ded
Rodrigo Costa <rodrigo.costa94@yahoo.com.br>
2025-03-20 13:12:48 -0300

    view criada para retornar o totalPrice do pedido ao usuario

--- orders/urls.py
+++ orders/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('orders/', views.OrderCreateListView.as_view(), name="order-create-list"),
+    path('orders/<int:pk>/', views.OrderRetrieveUpdateDestroyView.as_view(), name='order-detail-view'),
+    path('order/total-price/<int:order_number>/', views.OrderTotalPriceView.as_view(), name='order-total-price'),
 ]
--- orders/views.py
+++ orders/views.py
@@ -3,6 +3,11 @@ from orders.models import Order
 from rest_framework import generics
 from orders.serializer import OrderSerializer
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+
 
 class OrderCreateListView(generics.ListCreateAPIView):
     queryset = Order.objects.all()
@@ -12,3 +17,12 @@ class OrderCreateListView(generics.ListCreateAPIView):
 class OrderRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    
+    
+class OrderTotalPriceView(APIView):
+    def get(self,request, order_number):
+        try:
+            order = Order.objects.get(order_number= order_number)
+            return Response({"total_price":order.total_price}, status=status.HTTP_200_OK)
+        except Order.DoesNotExist:
+            return Response({"detail":"Order not found"}, status=status.HTTP_404_NOT_FOUND)
